2563482f773f9b0b7f8ae444abb126eefa96316d
Christian Urbanek <curbanek@cs.uchicago.edu>
2022-02-14 20:04:44 -0600

    We chilling

--- joystick/joystick.ino
+++ joystick/joystick.ino
@@ -1,18 +1,61 @@
 #define joyX A0
 #define joyY A1
-int xValue, yValue;
+int x, y;
+const int redPin1 = 7;
+const int blackPin1 = 6;
+const int redPin2 = 5;
+const int blackPin2 = 4;
  
 void setup() {
   Serial.begin(9600);
+  pinMode(redPin1, OUTPUT);
+  pinMode(blackPin1, OUTPUT);
+  pinMode(redPin2, OUTPUT);
+  pinMode(blackPin2, OUTPUT);
 }
  
 void loop() {
   // put your main code here, to run repeatedly:
-  xValue = analogRead(joyX);
-  yValue = analogRead(joyY);
+  x = analogRead(joyX);
+  y = analogRead(joyY);
+
+  // Down: clockwise
+  if (y > 550 && (x < 550 && x > 450)) {
+    digitalWrite(redPin1, HIGH);
+    digitalWrite(blackPin1, LOW);
+    digitalWrite(redPin2, HIGH);
+    digitalWrite(blackPin2, LOW);
+  }
+  // Up: counterclockwise
+  else if (y < 450 && (x < 550 && x > 450)) {
+    digitalWrite(redPin1, LOW);
+    digitalWrite(blackPin1, HIGH);
+    digitalWrite(redPin2, LOW);
+    digitalWrite(blackPin2, HIGH);
+  }
+  // Left: 1 clockwise, 2 counterclockwise
+  else if (x > 550 && (y < 550 && y > 450)) {
+    digitalWrite(redPin1, HIGH);
+    digitalWrite(blackPin1, LOW);
+    digitalWrite(redPin2, LOW);
+    digitalWrite(blackPin2, HIGH);
+  }
+  // Right: 1 counterclockwise, 2 clockwise
+  else if (x < 450 && (y < 550 && y > 450)) {
+    digitalWrite(redPin1, LOW);
+    digitalWrite(blackPin1, HIGH);
+    digitalWrite(redPin2, HIGH);
+    digitalWrite(blackPin2, LOW);
+  }
+  else {
+    digitalWrite(redPin1, LOW);
+    digitalWrite(blackPin1, LOW);
+    digitalWrite(redPin2, LOW);
+    digitalWrite(blackPin2, LOW);
+  }
  
-  //print the values with to plot or view
-  Serial.print(xValue);
+  //print the s with to plot or view
+  Serial.print(x);
   Serial.print("\t");
-  Serial.println(yValue);
+  Serial.println(y);
 }
